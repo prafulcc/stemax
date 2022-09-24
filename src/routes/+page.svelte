01434ec0eabcad8a53745fa054f84d31232b0e8e
Praful <praful@tuta.io>
2022-09-24 23:40:30 +0100

    a to an

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
     <meta name="author" content="Stemax Consult Services">
-    <meta name="description" content="We specialise in providing occupational and environmental health services to businesses, and we also run a outpatient day care clinic catering towards individuals.">
+    <meta name="description" content="We specialise in providing occupational and environmental health services to businesses and we also run an outpatient day care clinic catering towards individuals.">
     <meta property="og:image" content="/favicon.png" />
     <title>Stemax Healthcare Consultancy & Clinic</title>
 </svelte:head>
@@ -46,8 +46,6 @@
     ]
 </script>
 
-
-
 <div class="bg-white">
 	<div class="max-w-2xl lg:max-w-7xl mx-auto pt-6 px-4 sm:py-6 sm:px-6 lg:px-8">
 		<div class="relative rounded-lg overflow-hidden">
@@ -77,13 +75,12 @@
 	<div class="max-w-2xl mx-auto py-6 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div class="mx-1 pb-5 border-b border-gray-200">
             <h3 class="text-lg leading-6 font-medium text-gray-900">120+ Healthcare Services Offered</h3>
-            <p class="mt-2 text-sm text-gray-500">Though we specialise in providing occupational and environmental health services to businesses, we also run a outpatient day care clinic catering towards individuals.</p>
+            <p class="mt-2 text-sm text-gray-500">Though we specialise in providing occupational and environmental health services to businesses and we also run an outpatient day care clinic catering towards individuals.</p>
         </div>
         <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8 pt-6">
             {#each services as service}
 			<div class="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden hover:bg-emerald-50 hover:border-gray-300">
                 <div class="aspect-w-3 aspect-h-4 bg-gray-200 sm:aspect-none h-24">
-                    <!-- <div class="absolute inset-0 bg-emerald-500 mix-blend-multiply opacity-10"></div> -->
                     <img src="{service.image}" alt="" class="w-full h-full object-center object-cover sm:w-full sm:h-full -z-20">
                 </div>
                 <div class="flex-1 p-4 space-y-2 flex flex-col">
@@ -91,7 +88,7 @@
                         <p class="text-base font-medium text-gray-900">{service.title}</p>
                     </div>
 					<div class="text-sm font-medium text-gray-900">
-                        <a target="_blank" href="#">
+                        <a target="_blank" href="/">
                             <span aria-hidden="true" class="absolute inset-0"></span>
                             <p class="text-sm italic text-emerald-800 ">{service.services.toString()} Services</p>
 							
@@ -138,8 +135,6 @@
 	</div>
 </div>
 
-
-
 <div class="bg-white">
 	<div class="max-w-2xl lg:max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <div class="space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0 pt-6">
@@ -158,9 +153,9 @@
                         <p class="pt-5 text-gray-500">He specialises in using both a conventional and integrative functional medicine approach.</p>
                         <p class="pt-5 text-gray-500">A fellow of the Royal Society of Public health, Chartered Management Institute and WHO Collaborating centre for Advocacy and Training in Pharmacovigilance. A member of British Medical Association, British Society for Ecological Medicine (BSEM), World Medical Association and The British Toxicology Society.</p>
                     </div>
-                    <ul role="list" class="flex space-x-5">
+                    <ul class="flex space-x-5">
                         <li>
-                            <a target="_blank" href="#" class="text-gray-400 hover:text-gray-500">
+                            <a target="_blank" href="/" class="text-gray-400 hover:text-gray-500">
                                 <span class="sr-only">Twitter</span>
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                                     <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
@@ -168,7 +163,7 @@
                             </a>
                         </li>
                         <li>
-                            <a target="_blank" href="#" class="text-gray-400 hover:text-gray-500">
+                            <a target="_blank" href="/" class="text-gray-400 hover:text-gray-500">
                                 <span class="sr-only">LinkedIn</span>
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
